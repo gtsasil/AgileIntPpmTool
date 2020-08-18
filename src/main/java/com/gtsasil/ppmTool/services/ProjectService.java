98d6551394e6dcd5ee6b5dd162959f3ff71bc120
gtsasil <gtsasil@gmail.com>
2020-08-18 22:28:34 +0100

    Delete Mapping

--- src/main/java/com/gtsasil/ppmTool/services/ProjectService.java
+++ src/main/java/com/gtsasil/ppmTool/services/ProjectService.java
@@ -36,5 +36,13 @@ public class ProjectService {
         return projectRepository.findAll();
     }
 
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null){
+            throw new ProjectIdException("Cannot find Project ID: '"+projectId+"'. This project does not exist!");
+        }
+        projectRepository.delete(project);
+    }
 
 }
