41ba1e5ba8333e8705db9add0c01c688cb3e4ce5
gtsasil <gtsasil@gmail.com>
2020-08-18 21:32:32 +0100

    Find all Mapping

--- src/main/java/com/gtsasil/ppmTool/repositories/ProjectRepository.java
+++ src/main/java/com/gtsasil/ppmTool/repositories/ProjectRepository.java
@@ -2,9 +2,13 @@ package com.gtsasil.ppmTool.repositories;
 
 import com.gtsasil.ppmTool.domain.Project;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 
+    Project findByProjectIdentifier(String prejectId);
+
     @Override
-    Iterable<Project> findAllById(Iterable<Long> iterable);
+    Iterable<Project> findAll();
 }
--- src/main/java/com/gtsasil/ppmTool/services/ProjectService.java
+++ src/main/java/com/gtsasil/ppmTool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.gtsasil.ppmTool.services;
 
 import com.gtsasil.ppmTool.domain.Project;
+import com.gtsasil.ppmTool.exception.ProjectIdException;
 import com.gtsasil.ppmTool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,28 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch(Exception e){
+            throw new ProjectIdException("Project Id '" + project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
     }
+
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null ){
+            throw new ProjectIdException("Project Id '" + projectId+"' does not exists");
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+
 }
